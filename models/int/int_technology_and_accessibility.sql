db96421a26f9197fcdb93adf0f86bf78a17b9de8
Lawan Subba <lawansubba@gmail.com>
2024-08-10 20:09:59 +0200

    Update of Int layer

--- models/int/int_technology_and_accessibility.sql
+++ models/int/int_technology_and_accessibility.sql
@@ -23,45 +23,41 @@ SELECT
     complaint_form_helpful,
     crisis_communication_confidence,
     CASE 
-        WHEN crisis_communication_source IN ('social media', 'internet and social media and broadcasting tv stations, newspaper and medias',
-                            'social network, telecommunication', 'through social media', 'whatsapp', 
-                            'social platforms, newspaper, internet', 'THROUGH USING SOCIAL MEDIA MOSTLY WHATSAPP AND FACEBOOK',
-                            'social net work', '1. Watching TV 2. Social Media', 'through whatsapp', 'Facebook', 
-                            'from interpreters, WhatsApp, messenger and Facebook', 'tv news, online and social media, and deaf group') 
-            THEN 'Social Media'
-        WHEN crisis_communication_source IN ('internet', 'in the internet', 'internet (news)', 'through the internet', 'Google search and hospital') 
-            THEN 'Internet'
-        WHEN crisis_communication_source IN ('newspaper', 'i read newspaper, internet, and tv', 'by newspaper or on social media phone',
-                            'news casters, news paper', 'tv, newspaper and internet', 'Through newspapers and other social medias') 
-            THEN 'Newspaper'
-        WHEN crisis_communication_source IN ('family', 'through my family via gesturing', 'family gesturing', 'my family (using gesturing)', 
-                            'social media, family and friends', 'through my son', 'my neighbors', 
-                            'I get it through my nursery neighbor', 'Through a friend') 
-            THEN 'Family/Friends'
-        WHEN crisis_communication_source IN ('tv', 'online, newspaper, tv', 'reading newspaper and tv', 'on phone and tv', 'news', 
-                            'through news, television, and whatsapp', 'tv and the internet', 'phone/ tv news', 'Tv news', 
-                            'tv but no interpreter/no communication') 
-            THEN 'TV'
-        WHEN crisis_communication_source IN ('hospital', 'a doctor', 'send text to doctor', 'information doctor', 'writing the doctor about my health',
-                            'hospital and health care', 'to get money to get to hospital', 'need money to go to hospitals',
-                            'Health dept', 'from the hospital', 'Doctor', 'from health websites', 'From healthcare website') 
-            THEN 'Health Care'
-        WHEN crisis_communication_source IN ('with the help of interpreter', 'I had paid myself for interpreter', 'Interpreter') 
-            THEN 'Interpreter'
-        WHEN crisis_communication_source IN ('school community or deaf chat', 'through school, deaf community, deaf gist',
-                            'through the awareness group - information through media', 'media and news', 
-                            'through hearing from news, eg whatsapp, deaf gist') 
-            THEN 'Community'
-        WHEN crisis_communication_source IN ('message writing or sending sms', 'by texting') 
-            THEN 'Written Communication'
-        WHEN crisis_communication_source IN ('not at all', 'none', 'No', 'no information', 'no information for us at all', 
-                            'don\'t know', 'never', 'nowhere and no information at all', 'no news for sometime', 
-                            'no interpretation', 'None. Only if I have pre knowledge of what happened', 'nobody', 
-                            'public announcement', 'mass media', 'I see information', 'consulation', 'malaria') 
-            THEN 'Other'
-        WHEN crisis_communication_source IN ('Phone', 'from my phone', 'through phone') 
-            THEN 'Phone'
-        ELSE 'Unknown'
+	    WHEN crisis_communication_source IS NULL THEN 'Uncertain'
+	
+	    WHEN crisis_communication_source LIKE '%TV%' OR crisis_communication_source LIKE '%television%' OR 
+            crisis_communication_source LIKE '%news%' OR crisis_communication_source LIKE '%media%' OR 
+            crisis_communication_source LIKE '%newspaper%' OR crisis_communication_source LIKE '%internet%' OR 
+            crisis_communication_source LIKE '%online%' 
+        THEN 'Media (TV, Newspaper, Radio, Internet)'
+	
+        WHEN crisis_communication_source LIKE '%social media%' OR crisis_communication_source LIKE '%Facebook%' OR 
+            crisis_communication_source LIKE '%WhatsApp%' OR crisis_communication_source LIKE '%Messenger%' OR 
+            crisis_communication_source LIKE '%social platform%' OR crisis_communication_source LIKE '%internet%' OR 
+            crisis_communication_source LIKE '%online%' 
+        THEN 'Social Media & Online Platforms'
+        
+        WHEN crisis_communication_source LIKE '%family%' OR crisis_communication_source LIKE '%son%' OR 
+            crisis_communication_source LIKE '%gesturing%' OR crisis_communication_source LIKE '%friends%' OR 
+            crisis_communication_source LIKE '%community%' OR crisis_communication_source LIKE '%neighbors%' OR 
+            crisis_communication_source LIKE '%deaf gist%' 
+        THEN 'Family, Friends, and Community'
+        
+        WHEN 
+            crisis_communication_source LIKE '%doctor%' OR crisis_communication_source LIKE '%hospital%' OR 
+            crisis_communication_source LIKE '%health dept%' OR crisis_communication_source LIKE '%healthcare%' OR 
+            crisis_communication_source LIKE '%interpreter%' 
+        THEN 'Healthcare Providers (Doctors, Hospitals, Health Departments)'
+        
+        WHEN crisis_communication_source LIKE '%none%' OR crisis_communication_source LIKE '%no information%' OR 
+            crisis_communication_source LIKE '%not at all%' OR crisis_communication_source LIKE '%nowhere%' OR 
+            crisis_communication_source LIKE '%never%' OR crisis_communication_source LIKE '%no news%' 
+        THEN 'Uncertain'
+        
+        WHEN crisis_communication_source LIKE '%don’t know%' OR crisis_communication_source LIKE '%don\'t know%' 
+        THEN 'Uncertain'
+        
+	    ELSE 'Other reasons'
     END AS crisis_communication_source
 FROM 
     {{ ref('stg_technology_and_accessibility') }}
